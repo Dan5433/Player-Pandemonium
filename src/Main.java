ea6af48f40d08a5af9cc773bf7016bc0c7e0ee5a
Dan5433 <grishkov.daniil@gmail.com>
2026-05-29 11:43:55 -0400

    Add jersey font

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public final class Main extends PApplet {
     public static void main(String[] args) {
@@ -9,11 +10,25 @@ public final class Main extends PApplet {
         fullScreen(P2D);
     }
 
+    //fonts
+    PFont pixelbit, jersey;
+
     public void setup() {
         rectMode(CORNERS);
+        loadFonts();
     }
 
     public void draw() {
         background(255);
+
+        textFont(jersey);
+        textSize(32);
+        fill(0);
+        text("testing", mouseX, mouseY);
+    }
+
+    private void loadFonts() {
+        pixelbit = createFont("fonts/Pixelbit.ttf", 32);
+        jersey = createFont("fonts/jersey10/Jersey10-Regular.ttf", 32);
     }
 }
