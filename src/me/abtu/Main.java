4655a148f304fd8537e7e464bb58f3dff5b4507a
Dan5433 <grishkov.daniil@gmail.com>
2026-05-29 23:36:39 -0400

    Cache graphics into layers

--- src/me/abtu/Main.java
+++ src/me/abtu/Main.java
@@ -4,6 +4,7 @@ import me.abtu.scenes.Scene;
 import me.abtu.scenes.TitleScreen;
 import processing.core.PApplet;
 import processing.core.PFont;
+import processing.core.PGraphics;
 import processing.opengl.PGraphicsOpenGL;
 
 public final class Main extends PApplet {
@@ -18,32 +19,30 @@ public final class Main extends PApplet {
     // fonts
     public PFont pixelbit, jersey;
 
-    //scenes
-    private Scene[] scenes;
+    private PGraphics ui;
 
 
     public void setup() {
+        ((PGraphicsOpenGL) g).textureSampling(3); //force point texture sampling
+
         rectMode(CORNERS);
         loadFonts();
-        loadScenes();
 
-        ((PGraphicsOpenGL) g).textureSampling(3); //force point texture sampling
+        initializeGraphics();
+    }
+
+    private void initializeGraphics() {
+        ui = new TitleScreen().getGraphics(this);
     }
 
     public void draw() {
         background(255);
 
-        image(scenes[0].getGraphics(this), 0, 0);
+        image(ui, 0, 0);
     }
 
     private void loadFonts() {
         pixelbit = createFont("fonts/Pixelbit.ttf", 32, false);
         jersey = createFont("fonts/jersey10/Jersey10-Regular.ttf", 32, false);
-    }
-
-    private void loadScenes() {
-        scenes = new Scene[]{
-                new TitleScreen()
-        };
     }
 }
