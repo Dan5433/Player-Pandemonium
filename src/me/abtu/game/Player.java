010eb165384af0f5bcc19406423923d6a2878faf
Dan5433 <grishkov.daniil@gmail.com>
2026-06-01 12:06:40 -0400

    Display player keybinds

--- src/me/abtu/game/Player.java
+++ src/me/abtu/game/Player.java
@@ -1,21 +1,8 @@
 package me.abtu.game;
 
 
-public class Player {
-    //controls
-    private final int left;
-    private final int right;
-    private final int up;
-    private final int down;
-    private final int primary;
-    private final int secondary;
-
-    public Player(int up, int down, int left, int right, int primary, int secondary) {
-        this.up = up;
-        this.down = down;
-        this.left = left;
-        this.right = right;
-        this.primary = primary;
-        this.secondary = secondary;
-    }
+/**
+ * @param jump controls
+ */
+public record Player(int jump, int left, int right, int primary, int secondary) {
 }
--- src/me/abtu/graphics/ui/PlayerMenu.java
+++ src/me/abtu/graphics/ui/PlayerMenu.java
@@ -17,8 +17,8 @@ public class PlayerMenu extends GraphicsBuffer {
     public PlayerMenu(PApplet app, int resizeMode) {
         super(app, resizeMode);
 
-        players.add(new Player(KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_A, KeyEvent.VK_D, KeyEvent.VK_Q, KeyEvent.VK_E));
-        players.add(new Player(KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_SHIFT, KeyEvent.VK_CONTROL));
+        players.add(new Player(KeyEvent.VK_W, KeyEvent.VK_A, KeyEvent.VK_D, KeyEvent.VK_Q, KeyEvent.VK_E));
+        players.add(new Player(KeyEvent.VK_UP, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_SHIFT, KeyEvent.VK_CONTROL));
     }
 
     @Override
@@ -34,14 +34,37 @@ public class PlayerMenu extends GraphicsBuffer {
         graphics.rectMode(PConstants.CENTER);
 
         graphics.fill(255);
-        final float cardMargin = 4f;
+        final float cardMargin = REFERENCE_WIDTH / 64f;
         final float cardWidth = REFERENCE_WIDTH / 6.4f;
         final float cardHeight = REFERENCE_HEIGHT / 2.4f;
         int numPlayers = players.size();
         for (int i = 0; i < numPlayers; i++) {
             float offset = i - (numPlayers - 1) / 2f;
-            int cardX = (int) (HALF_WIDTH + offset * (cardWidth + cardMargin));
+            float cardX = HALF_WIDTH + offset * (cardWidth + cardMargin);
+            graphics.fill(255);
             graphics.rect(cardX, HALF_HEIGHT, cardWidth, cardHeight);
+
+
+            graphics.translate(cardX, HALF_HEIGHT - cardHeight / 2f);
+
+            graphics.fill(0);
+            graphics.textFont(main.getDefaultFont());
+            graphics.textSize(SMALL_TEXT_SIZE);
+
+            final float textMargin = 2;
+            final float textLineSpacing = 16;
+            graphics.textAlign(PConstants.CENTER, PConstants.TOP);
+            graphics.text("Player " + (i + 1), 0, textMargin);
+
+            graphics.translate(-cardWidth / 2f, textLineSpacing);
+            graphics.textAlign(PConstants.LEFT, PConstants.TOP);
+            graphics.text("Jump: " + KeyEvent.getKeyText(players.get(i).jump()), textMargin, textMargin);
+            graphics.text("Left: " + KeyEvent.getKeyText(players.get(i).left()), textMargin, textMargin + textLineSpacing);
+            graphics.text("Right: " + KeyEvent.getKeyText(players.get(i).right()), textMargin, textMargin + textLineSpacing * 2);
+            graphics.text("Primary: " + KeyEvent.getKeyText(players.get(i).primary()), textMargin, textMargin + textLineSpacing * 3);
+            graphics.text("Secondary: " + KeyEvent.getKeyText(players.get(i).secondary()), textMargin, textMargin + textLineSpacing * 4);
+
+            graphics.translate(-(cardX - cardWidth / 2f), -(HALF_HEIGHT - cardHeight / 2f + textLineSpacing));
         }
     }
 }
