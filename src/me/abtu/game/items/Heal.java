3485cbd67007d6c6289bec7f989672d0c3c49bee
Dan5433 <grishkov.daniil@gmail.com>
2026-06-15 14:26:27 -0400

    Add medkit sprite

--- src/me/abtu/game/items/Heal.java
+++ src/me/abtu/game/items/Heal.java
@@ -2,13 +2,12 @@ package me.abtu.game.items;
 
 import me.abtu.game.entity.player.Player;
 import processing.core.PApplet;
-import processing.core.PImage;
 
 public class Heal extends Item {
     private final float healAmount;
 
     public Heal(float x, float y, PApplet app) {
-        super(x, y, new PImage());
+        super(x, y, app.loadImage("sprites/items/medkit.png"));
         healAmount = app.random(9f, 12f);
     }
 
